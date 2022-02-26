164af249c73a8e61a7e752d73b939d11eff70eea
Stanley Thijssen <stanley.thijssen@gmail.com>
2022-02-26 17:13:48 +0800

    feat: added support for environment variables from the google and weather apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export const App = () => {
-  return <div>App wrapper</div>
+  return (
+    <div>
+      {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      <br />
+      {process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}
+    </div>
+  )
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="react-scripts" />
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_GOOGLE_MAPS_API_KEY: string
+    REACT_APP_OPEN_WEATHER_MAP_API_KEY: string
+    NODE_ENV: 'development' | 'production' | 'test'
+  }
+}
